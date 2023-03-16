543cd999edcd7758a4f172cdb885adcd7f267a7a
faculeogimenez <126607123+faculeogimenez@users.noreply.github.com>
2023-03-16 15:08:23 -0300

    Add files via upload

---
+++ mijavascript.js
@@ -0,0 +1,19 @@
+document.getElementById('botoneducacion').onclick=function () {
+    document.getElementById("educacion").innerHTML="ISFDyT No213 • 2021-Actualidad Tecnicatura en Domotica y Seguridad Electronica.Cfp No10 • 2020-2020 Cert. Electricista Instalador Escuela Educacion Media No1 • Egreso. 2006 Bachiller en Economia y Gestion"
+}
+
+document.getElementById('botonexperiencia').onclick=function () {
+    document.getElementById("experiencia").innerHTML="Havanna S.A • 2007-Actualidad Encargado de tienda Manejo de stock, finanzas y RRHH" + "• FG Instalaciones Electricas - Actualidad  Emprendimiento destinado a las instalaciones, proyecciones y reparaciones de instalaciones electricas"
+}
+
+document.getElementById('botondatospersonales').onclick=function () {
+    document.getElementById("datospersonales").innerHTML="Soy electricista y estudiante. Me apasionan los desafios y contribuir desde mi puesto para logros y metas."
+}
+
+document.getElementById('botonhabilidades').onclick=function () {
+    document.getElementById("habilidades").innerHTML="Actitud y desempeño proactivo. Ingles nivel medio Manejo y coordinacion de grupos. Manejo intermedio de AutoCad. Conocimiento de programacion en C++ Manejo fluido de Office."
+}
+
+
+
+	
